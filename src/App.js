e19dcda9e3180adf5e034559803aa35a09e0cfa1
kriptokingdm <kriptokingdm@gmail.com>
2025-11-10 14:38:58 +0300

    Fix React with createElement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+// src/App.js
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>✅ TetherBot РАБОТАЕТ!</h1>
-      <p>Если видишь этот текст - React работает</p>
-    </div>
+  return React.createElement('div', { 
+    style: { 
+      padding: '50px', 
+      textAlign: 'center',
+      backgroundColor: '#007cff',
+      color: 'white',
+      minHeight: '100vh'
+    } 
+  }, 
+    React.createElement('h1', null, '🎉 TETHERBOT РАБОТАЕТ!'),
+    React.createElement('p', null, 'React успешно загружен!'),
+    React.createElement('button', {
+      onClick: () => alert('Кнопка работает!')
+    }, 'Тестовая кнопка')
   );
 }
 
